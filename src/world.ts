6edd4fc11bcbe616559ffc48ac3e21fd4dd55dea
熊澤大河 <>
2024-01-19 20:39:46 +0000

    fix world.ts

--- src/world.ts
+++ src/world.ts
@@ -1,10 +1,8 @@
 export default class World {
     message: string;
-
     constructor(message: string) {
         this.message = message;
     }
-
     public sayHello(elem: HTMLElement | null) {
         if (elem) {
             elem.innerText = this.message
